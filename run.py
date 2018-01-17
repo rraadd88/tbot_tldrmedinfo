dee361f0495e9bfc9cafaac53daa1de3017d5ec2
rraadd88 <rraadd_8@hotmail.com>
2018-01-17 23:07:30 +0530

    tmp added

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from modules import auth, send_healthpng
 from keys import params_auth
+from tmp import since_id
 api=auth(params_auth)
-since_id='953680302634758145'
+# since_id='953680302634758145'
 while True:
     twits=api.search(q='@tldrmedinfo',
     #                  lang='en',
@@ -14,3 +15,5 @@ while True:
             if not 'FYI' in t.text: 
                 send_healthpng(api,t)
         since_id=twits[0].id
+        with open('tmp.py', 'w') as f:
+            f.write("since_id='{}'".format(since_id))
